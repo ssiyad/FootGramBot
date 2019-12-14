69ad831fe624688cd9240b356e73c606506f3c1c
Sabu Siyad <ssiyad@tuta.io>
2019-12-14 21:04:31 +0530

    Public repo

--- FootGramBot/modules/start.py
+++ FootGramBot/modules/start.py
@@ -6,8 +6,17 @@ from FootGramBot import dp
 
 @run_async
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Hello there! I'm a bot made by @ssiyad . Feel free to contact him for any assistance")
+
+    START_MSG = '''
+Hello there! I'm bot written in python by @ssiyad . Please pass any feedback to him!
+
+If you'r interested in my source code, it is available at https://github.com/ssiyad/FootGramBot
+
+You can get some help by using `/help` command.
+
+Please help me evolve and keep #Love #Football ;)'''
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=START_MSG, parse_mode='MARKDOWN')
 
 
 dp.add_handler(CommandHandler('start', start, filters=Filters.private))
