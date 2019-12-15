b358083474e360f0f528099bf897804d84b79c3c
Sabu Siyad <ssiyad@tuta.io>
2019-12-15 22:10:53 +0530

    Init

---
+++ FootGramBot/modules/helpers/database.py
@@ -0,0 +1,49 @@
+import datetime
+
+import peewee
+
+db = peewee.SqliteDatabase("db.db")
+
+
+class Match(peewee.Model):
+    id = peewee.PrimaryKeyField()
+    match_id = peewee.IntegerField()
+    stage = peewee.CharField()
+    group = peewee.CharField()
+    winner = peewee.CharField()
+    duration = peewee.CharField()
+    full_home = peewee.IntegerField()
+    full_away = peewee.IntegerField()
+    half_home = peewee.IntegerField()
+    half_away = peewee.IntegerField()
+    extra_home = peewee.IntegerField(default=None)
+    extra_away = peewee.IntegerField(default=None)
+    pen_home = peewee.IntegerField(default=None)
+    pen_away = peewee.IntegerField(default=None)
+    home = peewee.CharField()
+    away = peewee.CharField()
+    updated = peewee.DateField(default=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+    class Meta:
+        database = db
+        table_name = 'matches'
+
+
+class Live(peewee.Model):
+    id = peewee.PrimaryKeyField()
+    league = peewee.CharField()
+    time = peewee.CharField()
+    goals_home = peewee.IntegerField()
+    goals_away = peewee.IntegerField()
+    home = peewee.CharField()
+    away = peewee.CharField()
+    updated = peewee.DateField(default=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+    class Meta:
+        database = db
+        table_name = 'lives'
+
+
+# create_tables
+Match.create_table()
+Live.create_table()
