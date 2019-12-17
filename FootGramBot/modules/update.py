2f5e1ac437e1eb03306875952173992fb4af2194
Sabu Siyad <ssiyad@tuta.io>
2019-12-17 15:18:11 +0530

    Add utcDate

--- FootGramBot/modules/update.py
+++ FootGramBot/modules/update.py
@@ -14,6 +14,7 @@ def update_matches():
                 d = {'match_id': match['id'],
                      'comp': comp,
                      'matchday': match['season']['currentMatchday'],
+                     'date_utc': match['utcDate'],
                      'status': match['status'],
                      'stage': match['stage'],
                      'group': match['group'],
